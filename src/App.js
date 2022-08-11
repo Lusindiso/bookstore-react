7895c3f39d7bc7d265dde73818e9dccea68a1ce5
lUCI <lusindisontanjana@gmail.com>
2022-08-11 15:20:28 +0200

    setup react-router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Books from './components/pages/Books';
+import Categories from './components/pages/Categories';
+import Header from './components/UI/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/" element={<Books />} />
+      </Routes>
+    </>
+
   );
 }
 
---
+++ src/components/Book.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import classes from './Book.module.css';
+
+const Book = ({ title, author }) => (
+  <div className={classes.book}>
+    <h3 className="title">{title}</h3>
+    <div className="author">{author}</div>
+    <div className={classes.buttons}>
+      <button type="button">Comments</button>
+      <button type="button">Remove</button>
+      <button type="button">Edit</button>
+    </div>
+  </div>
+);
+
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
+
+export default Book;
