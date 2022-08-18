8ff63838b924b099ee0e9d50fb33398361ea7911
Lusindiso Ntanjana <31928865+Lusindiso@users.noreply.github.com>
2022-08-18 13:04:30 +0200

    Add books initial state

--- src/components/pages/Categories.js
+++ src/components/pages/Categories.js
@@ -1,6 +1,17 @@
-const Categories = () => (
-  <main>
-    <button type="button">Check Status</button>
-  </main>
-);
+import { useDispatch } from 'react-redux';
+import { categoriesActions } from '../../redux/categories/categories';
+
+const Categories = () => {
+  const dispatch = useDispatch();
+
+  const clickHandler = () => {
+    dispatch(categoriesActions.checkStatus());
+  };
+  return (
+    <main>
+      <button type="button" onClick={clickHandler}>Check Status</button>
+    </main>
+  );
+};
+
 export default Categories;
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,19 +1,31 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+  books: [
+    {
+      id: '255459688',
+      author: 'Frank Herbit',
+      title: 'Dune',
+    },
+    {
+      id: '20322515231530',
+      author: 'Suzanne Collins',
+      title: 'Capital in the Twenty-First Century',
+    },
+  ],
+};
 const books = createSlice({
   name: 'book',
-  initialState: { books: [] },
+  initialState,
   reducers: {
     addBook(state, action) {
-      state.push(action.payload.book);
+      state.books.push(action.payload);
     },
     removeBook(state, action) {
       // eslint-disable-next-line no-param-reassign
-      state.books = state.books.filter((book) => book.id !== action.payload.id);
+      state.books = state.books.filter((book) => book.id !== action.payload);
     },
   },
 });
-
 export const booksActions = books.actions;
-
 export default books;
