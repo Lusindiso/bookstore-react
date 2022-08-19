06f69cabedb1bc00a9b8523f0b1320ba3f119fa3
Lusindiso Ntanjana <31928865+Lusindiso@users.noreply.github.com>
2022-08-19 10:17:21 +0200

    Display data from API

--- src/components/UI/AddBook.js
+++ src/components/UI/AddBook.js
@@ -1,42 +1,48 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { booksActions } from '../../redux/books/books';
+import { sendBooksData } from '../../redux/books/booksActions';
 import classes from './AddBook.module.css';
 
 const AddBook = () => {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-
-  const titleChangeHandler = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const authorChangeHandler = (e) => {
-    setAuthor(e.target.value);
-  };
-
-  const dispatch = useDispatch();
+	@@ -19,11 +20,17 @@ const AddBook = () => {
   const submitHandler = (e) => {
     e.preventDefault();
     if (title.length > 2 && author.length > 2) {
-      dispatch(booksActions.addBook({
+      const newBook = {
         id: Math.random().toString(),
         title,
         author,
-      }));
+        category: 'Action',
+      };
+      dispatch(sendBooksData(newBook));
+      const key = newBook.id;
+      const obj = {};
+      obj[key] = [newBook];
+      dispatch(booksActions.addBook(obj));
       setTitle('');
       setAuthor('');
     }
-  };
-  return (
+	@@ -32,8 +39,18 @@ const AddBook = () => {
     <div className={classes.addbook}>
       <h2>ADD NEW BOOK</h2>
       <form onSubmit={submitHandler}>
-        <input type="text" placeholder="Book title" onChange={titleChangeHandler} value={title} />
-        <input type="text" placeholder="Author" onChange={authorChangeHandler} value={author} />
+        <input
+          type="text"
+          placeholder="Book title"
+          onChange={titleChangeHandler}
+          value={title}
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          onChange={authorChangeHandler}
+          value={author}
+        />
         <button type="submit">Add Book</button>
       </form>
     </div>
   );
 };
+
 export default AddBook;
--- src/components/pages/Books.js
+++ src/components/pages/Books.js
@@ -1,24 +1,39 @@
-import { useSelector } from 'react-redux';
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable guard-for-in */
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchBooksData } from '../../redux/books/booksActions';
 import Book from '../Book';
 import AddBook from '../UI/AddBook';
 
 const Books = () => {
+  const dispatch = useDispatch();
   const books = useSelector((state) => state.books.books);
+  useEffect(() => {
+    dispatch(fetchBooksData());
+  }, [dispatch]);
+
+  const bookList = [];
+
+  for (const key in books) {
+    bookList.push(
+      <Book
+        key={key}
+        id={key}
+        author={books[key][0].author}
+        title={books[key][0].title}
+      />,
+    );
+  }
 
   return (
     <main>
       <div className="container">
-        {books.map((book) => (
-          <Book
-            key={book.id}
-            id={book.id}
-            author={book.author}
-            title={book.title}
-          />
-        ))}
+        {bookList}
         <AddBook />
       </div>
     </main>
   );
 };
+
 export default Books;
